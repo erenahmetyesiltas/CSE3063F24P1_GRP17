d8c6408e5d2b9a8d5d334aee98664d1d54d13d32
Ilker Yetimoglu <ilkeryetimoglu182@gmail.com>
2024-11-12 00:42:38 +0300

    An alternative main has been added as Main 2

--- src/Main2.java
+++ src/Main2.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
 
         try {
@@ -12,141 +12,124 @@ public class Main {
             LoginSystem loginSystem = new LoginSystem();
             Scanner scanner = new Scanner(System.in);
 
+            System.out.println("Select user type:");
+            System.out.println("1 - Student");
+            System.out.println("2 - Advisor");
+            System.out.print("Enter your choice: ");
+            int userType = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
 
+            switch (userType) {
+                case 1 -> {
+                    System.out.print("Enter your nickname: ");
+                    String nickname = scanner.nextLine();
 
-            System.out.print("Enter your nickname: ");
-            String nickname = scanner.nextLine();
+                    System.out.print("Enter your password: ");
+                    String password = scanner.nextLine();
 
-            System.out.print("Enter your password: ");
-            String password = scanner.nextLine();
-
-            try {
-                //Authenticate the Student
-                if (loginSystem.authenticateUser(nickname, password, system)) {
-                    System.out.println("Login successful!");
-                    if (loginSystem.getStudent() != null) {
+                    if (loginSystem.authenticateUser(nickname, password, system) && loginSystem.getStudent() != null) {
                         student = loginSystem.getStudent();
+                        System.out.println("Login successful as Student!");
 
                         while (true) {
                             System.out.println("----------ACTIONS----------");
                             System.out.println("1- Create a registration");
                             System.out.println("2- Check current registration status");
-                            System.out.println("\nPlease choose an action :");
-
+                            System.out.print("\nPlease choose an action: ");
                             int choice = scanner.nextInt();
 
                             switch (choice) {
-                                case 1:
-
+                                case 1 -> {
                                     system.printSuitableCourses();
-
-                                    while (true){
-
-                                        System.out.println("\nDo you want to add courses? (y/n): ");
+                                    while (true) {
+                                        System.out.print("\nDo you want to add courses? (y/n): ");
                                         String addCourse = scanner.next();
                                         if (addCourse.equalsIgnoreCase("y")) {
                                             system.readCourses(student);
-                                        }
-                                        else if (addCourse.equalsIgnoreCase("n")) {
+                                        } else if (addCourse.equalsIgnoreCase("n")) {
                                             break;
                                         }
                                     }
 
-                                    System.out.println("The courses inside your registration are :");
+                                    System.out.println("The courses inside your registration are:");
                                     for (CourseSection courseSection : student.getRegistration().getCourseSectionList()) {
                                         System.out.println(courseSection.getCourse().getId() + " - " + courseSection.getSectionNumber());
                                     }
 
                                     System.out.println("\n----------System is checking eligibility----------\n");
 
-                                    // Buraya checkEligibility() gelecek daha implement edilmedi
+                                    // checkEligibility() method to be implemented
 
-                                    System.out.println("Are you sure want to send the registration request to your advisor? (y/n): ");
-
+                                    System.out.print("Are you sure you want to send the registration request to your advisor? (y/n): ");
                                     String requestChoice = scanner.next();
                                     if (requestChoice.equalsIgnoreCase("y")) {
                                         system.sendRegistrationToAdvisor(student.getRegistration(), student);
-                                        System.out.println("SUCCESS : The registration request have sent to your advisor\n");
-                                        System.out.println();
-                                        break;
-                                    }
-                                    else if (requestChoice.equalsIgnoreCase("n")) {
+                                        System.out.println("SUCCESS: The registration request has been sent to your advisor\n");
+                                    } else {
                                         student.setRegistration(null);
-                                        System.out.println("WARNING : The registration you have been created is deleted. Make a new one\n");
-                                        break;
+                                        System.out.println("WARNING: The registration you created has been deleted. Make a new one\n");
                                     }
-
-                                case 2 :
+                                }
+                                case 2 -> {
                                     System.out.println();
                                     system.studentEnrollRequest(student);
+                                }
+                                default -> System.out.println("Invalid choice. Please try again.");
                             }
 
-                            System.out.println("Do you want to continue ? (If not you will logout) (y/n): ");
+                            System.out.print("Do you want to continue? (If not, you will logout) (y/n): ");
                             String continueChoice = scanner.next();
-                            if (continueChoice.equalsIgnoreCase("y")) {
-                                continue;
-                            }
-                            else if (continueChoice.equalsIgnoreCase("n")) {
-                                // En son state'i json'a kaydetme yazılacak yetiştiremedim
+                            if (continueChoice.equalsIgnoreCase("n")) {
                                 system.saveLastState();
-                                System.out.println("\nLogged out successfully! ");
+                                System.out.println("\nLogged out successfully!");
                                 break;
                             }
-
-
                         }
-
-
+                    } else {
+                        System.out.println("Invalid nickname or password.");
                     }
-                    else {
-                        // Advisor işlemleri ve UI burada
+                }
+                case 2 -> {
+                    System.out.print("Enter your nickname: ");
+                    String nickname = scanner.nextLine();
+
+                    System.out.print("Enter your password: ");
+                    String password = scanner.nextLine();
+
+                    if (loginSystem.authenticateUser(nickname, password, system) && loginSystem.getAdvisor() != null) {
                         advisor = loginSystem.getAdvisor();
+                        System.out.println("Login successful as Advisor!");
+
                         while (true) {
                             System.out.println("----------ADVISOR ACTIONS----------");
                             System.out.println("1- View students");
                             System.out.println("2- Approve/Reject registration requests");
                             System.out.println("3- Log out");
                             System.out.print("Please choose an action: ");
-
                             int advisorChoice = scanner.nextInt();
-                            scanner.nextLine(); // Consume newline
 
                             switch (advisorChoice) {
                                 case 1 -> {
                                     System.out.println("Students under your supervision:");
-                                    /*
-                                    for (Student supervisedStudent : advisor.getStudents()) {
-                                        System.out.println("ID: " + supervisedStudent.getId() + " | Name: " + supervisedStudent.getName());
-                                    }
-
-                                     */
+                                    // Implement the logic for listing students
                                 }
                                 case 2 -> {
-                                    System.out.println("IT'S NOT WORKING");
-                                    //Will be in iteration 2
-                                    //system.answerRegistrationRequests(advisor);
+                                    System.out.println("Approve/Reject registration requests - Not implemented yet.");
                                 }
                                 case 3 -> {
                                     system.saveLastState();
                                     System.out.println("Logging out...");
-                                    return; // Exit the advisor actions loop
+                                    return;
                                 }
                                 default -> System.out.println("Invalid choice. Please try again.");
                             }
                         }
+                    } else {
+                        System.out.println("Invalid nickname or password.");
                     }
-                } else {
-                    System.out.println("Invalid nickname or password.");
                 }
-
-
-            } catch (IOException e) {
-                System.out.println("An error occurred: " + e.getMessage());
-            } catch (Exception e) {
-            e.printStackTrace();
+                default -> System.out.println("Invalid user type. Please restart and select a valid option.");
             }
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
