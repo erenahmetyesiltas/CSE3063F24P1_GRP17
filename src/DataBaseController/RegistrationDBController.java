6b8dfab6b457deb0d658a75af5bcdef7dee98656
Eren Ahmet Yesiltas <erenahmetyslts@gmail.com>
2024-12-01 20:36:27 +0300

    Indeting problem about registration json files are fixed.

--- src/DataBaseController/RegistrationDBController.java
+++ src/DataBaseController/RegistrationDBController.java
@@ -1,6 +1,7 @@
 package DataBaseController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import main.Advisor;
 import main.DataHandler;
 import main.Registration;
@@ -43,6 +44,8 @@ public class RegistrationDBController {
     }
 
     public void updateRegistrations(Registration registrationGiven,int status){
+
+        //objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
         URL registrationMainPath = RegistrationDBController.class.getClassLoader().getResource("database/registrations/"+registrationGiven.getId()+".json");
         File willUpdateRegistrationFile;
