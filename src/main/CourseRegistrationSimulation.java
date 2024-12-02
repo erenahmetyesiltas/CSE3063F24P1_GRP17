52edb30f2902efbb7bce04758ee2f647d304f965
Eren Ahmet Yesiltas <erenahmetyslts@gmail.com>
2024-12-02 22:12:59 +0300

    User menu returning option is removed for all the user types.

--- src/main/CourseRegistrationSimulation.java
+++ src/main/CourseRegistrationSimulation.java
@@ -105,20 +105,25 @@ class CourseRegistrationSimulation {
                 System.out.println("1- Create a registration");
                 System.out.println("2- Check current registration status");
                 System.out.println("3- Print weekly schedule");
-                System.out.println("4- Turn back to the user selection menu");
+                //System.out.println("4- Turn back to the user selection menu");
                 System.out.print("Please choose an action: ");
 
                 int choice = scanner.nextInt();
+
+                while(choice < 1 || choice > 3){
+                    System.out.print("Please type a invalid choice: ");
+                    choice = scanner.nextInt();
+                }
 
                 switch (choice) {
                     case 1 -> createRegistration(student);
                     case 2 -> courseRegSystem.getStudentRegistrationStatus(student);
                     case 3 -> student.printWeeklyScheduleAsTable(student);
-                    case 4 -> run();
+                    //case 4 -> run();
                     default -> System.out.println("Invalid choice.");
                 }
 
-                System.out.println("Do you want to continue? (If not you will logout) (y/n): ");
+                System.out.print("Do you want to continue? (If not you will logout) (y/n): ");
                 String continueChoice = scanner.next();
                 if (continueChoice.equalsIgnoreCase("n")) {
                     logout();
@@ -203,12 +208,12 @@ class CourseRegistrationSimulation {
                 System.out.println("----------ACTIONS----------");
                 //System.out.println("1- Check students");
                 System.out.println("1- Approve/Reject student registration requests");
-                System.out.println("2- Turn back to the user selection menu");
+                //System.out.println("2- Turn back to the user selection menu");
                 System.out.print("Please choose an action: ");
 
                 int choice = scanner.nextInt();
 
-                while (choice != 1 && choice != 2) {
+                while (choice != 1) {
                     System.out.println();
                     System.out.print("Please enter a valid option:");
                     choice = scanner.nextInt();
@@ -217,7 +222,7 @@ class CourseRegistrationSimulation {
                 switch (choice) {
                     //case 1 -> checkStudents(advisor);
                     case 1 -> handleRegistrationRequests(advisor);
-                    case 2 -> run();
+                    //case 2 -> run();
                 }
 
                 System.out.print("Do you want to continue? (If not you will logout) (y/n): ");
@@ -298,13 +303,13 @@ class CourseRegistrationSimulation {
         while (true) {
             System.out.println();
             System.out.println("----------ACTIONS----------");
-            System.out.println("1- Assign time to the course sections of the department");
-            System.out.println("2- Turn back to the user selection menu");
+            System.out.println("1- Assign time and classroom to the course sections of the department");
+            //System.out.println("2- Turn back to the user selection menu");
             System.out.print("Please choose an action: ");
 
             int choice = scanner.nextInt();
 
-            while(choice < 1 || choice > 2){
+            while(choice != 1){
                 System.out.println();
                 System.out.print("Please enter a valid option:");
                 choice = scanner.nextInt();
@@ -313,7 +318,7 @@ class CourseRegistrationSimulation {
             switch (choice) {
                 //case 1 -> checkStudents(advisor);
                 case 1 -> handleCourseSectionTimesAndClassroom(departmentScheduler);
-                case 2 -> run();
+                //case 2 -> run();
             }
 
             System.out.print("Do you want to continue? (If not you will logout) (y/n): ");
