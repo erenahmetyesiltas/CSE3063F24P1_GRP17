1c3474354d23d881716f70b0f2da01abb5af22d2
Enes Berk Yetim <110656328+enesberkyetim@users.noreply.github.com>
2024-12-15 18:39:01 +0300

    CourseSection, Lecturer and Classroom.py are created and filled

---
+++ main/Classroom.py
@@ -0,0 +1,20 @@
+
+class Classroom :
+    __id : str
+    __capacity : int
+
+    def __init__(self):
+        pass
+
+    def getId(self):
+        return self.__id
+
+    def setId(self, id):
+        self.__id = id
+
+    def getCapacity(self):
+        return self.__capacity
+
+    def setCapacity(self, capacity):
+        self.__capacity = capacity
+
