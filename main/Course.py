9f644a65f5bcc34dc23f6d867e0e54e5b3d12deb
Enes Berk Yetim <110656328+enesberkyetim@users.noreply.github.com>
2024-12-15 18:18:24 +0300

    Course.py is filled

--- main/Course.py
+++ main/Course.py
@@ -14,3 +14,42 @@ class Course :
 
     def getId(self):
         return self.__id
+
+    def setId(self, id):
+        self.__id = id
+
+    def getCredit(self):
+        return self.__credit
+
+    def setCredit(self, credit):
+        self.__credit = credit
+
+    def getDepartments(self):
+        return self.__departments
+
+    def setDepartments(self, departments):
+        self.__departments = departments
+
+    def getTerm(self):
+        return self.__term
+
+    def setTerm(self, term):
+        self.__term = term
+
+    def getPrerequisiteCourses(self):
+        return self.__prerequisiteCourses
+
+    def setPrerequisiteCourses(self, prerequisiteCourses):
+        self.__prerequisiteCourses = prerequisiteCourses
+
+    def getName(self):
+        return self.__name
+
+    def setName(self, name):
+        self.__name = name
+
+    def getWeeklyCourseHours(self):
+        return self.__weeklyCourseHours
+
+    def setWeeklyCourseHours(self, weeklyCourseHours):
+        self.__weeklyCourseHours = weeklyCourseHours
