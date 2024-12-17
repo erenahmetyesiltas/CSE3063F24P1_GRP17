c43653fa0f954640962cbaede20b8b98145e8762
Melisa <115430817+mlsarac@users.noreply.github.com>
2024-12-17 22:10:27 +0300

    reg2

--- main/Registration.py
+++ main/Registration.py
@@ -15,25 +15,26 @@ class Registration:
     def addCourseSection(self,courseSectionAdded):
         if(len(self.__courseSections) >= 5):
             print("WARNING ! : (Total course number exceeds 5" + courseSectionAdded.getCourse.getId() + " - " + courseSectionAdded.getSectionNumber() + " cannot added to registration")
-
+            return False
         if (courseSectionAdded != None) :
             for courseSection in self.__courseSections :
                 if (courseSectionAdded.getCourse().getId().equals(courseSection.getCourse().getId()) and courseSectionAdded.getSectionNumber() == courseSection.getSectionNumber()):
                     print("WARNING ! : (Same course and section exists in your registration) " + courseSectionAdded.getCourseId() + " - " + courseSectionAdded.getSectionNumber() + " cannot added to registration")
-
+                    return False
 
                 elif (courseSectionAdded.getCourse().getId()==(courseSection.getCourse().getId()) and courseSectionAdded.getSectionNumber() != courseSection.getSectionNumber()):
                     print("WARNING ! : (You try to select a second section for the same course) " + courseSectionAdded.getCourseId() + " - " + courseSectionAdded.getSectionNumber() + " cannot added to registration")
-
+                    return False
 
 
 
             self.__courseSections.append(courseSectionAdded)
             print(courseSectionAdded.getCourse().getId() + " - " + courseSectionAdded.getSectionNumber() + " added to registration")
-
+            return True
 
         else:
             print("WARNING ! : " + courseSectionAdded.getCourseId() + " - " + courseSectionAdded.getSectionNumber() + " cannot added to registration")
+            return False
 
     def __init__(self):
         self._course_sections = []
