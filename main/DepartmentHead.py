5a324b45ae0a903c9308c199b41f467677890aee
ilkeery <110738491+ilkeery@users.noreply.github.com>
2024-12-22 20:24:39 +0300

    Working on Department Head

--- main/DepartmentHead.py
+++ main/DepartmentHead.py
@@ -1,1 +1,18 @@
-    
+class DepartmentHead():
+    __id : str
+    __firstName : str
+    __lastName : str
+    __password : str
+    __departmentName : str
+    __departmentId : str
+
+    def __init__(self, id: str, firstName: str, lastName: str, password: str, departmentName: str, departmentId: str):
+        self.__id = id
+        self.__firstName = firstName
+        self.__lastName = lastName
+        self.__password = password
+        self.__departmentName = departmentName
+        self.__departmentId = departmentId
+
+    def changeCourseCapacity():
+        pass
